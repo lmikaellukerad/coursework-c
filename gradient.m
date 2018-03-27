6e579828726a940ebb7e72e9b538c39f5fbc6630
Luke Prananta <m.l.r.prananta@student.tudelft.nl>
2018-03-27 09:32:10 +0200

    Add changes

--- gradient.m
+++ gradient.m
@@ -1,12 +1,45 @@
-function [ G ] = gradient( h, w )
+function [ G ] = gradient( h, w)
 %GRADIENT Summary of this function goes here
-%   Detailed explanation goes here
-i = zeros(2*h*(w-1)+2*w*(h-1),1);
-j = zeros(2*h*(w-1)+2*w*(h-1),1);
-v = zeros(2*h*(w-1)+2*w*(h-1),1);
+
+% Sharpening function using gradients only between vertical neighbours.
+
+x = [-1 1]
+y = [1 2]
+z = [1 h + 1]
+
+x_w = h*(w-1);
+x_h = w*(h-1);
+
+hj_1 = repmat(y, [1 x_h]);
+hj_2 = repmat(0:(x_h - 1), [2 1]);
+hj_2 = hj_2(:)';
+hj_3 = repmat(0:(w - 1), [(2*(h-1)) 1]);
+hj_3 = hj_3(:)';
+hj = hj_1 + hj_2 + hj_3;
+
+hi = repmat(1:x_h, [2 1]);
+hi = hi(:)';
+
+hv = repmat(x, [1 x_h]);
+
+wi = repmat(1:x_w, [2 1]);
+wi = wi(:)';
+
+wj_1 = repmat(z, [1 x_w]);
+wj_2 = repmat(0:h:(h*(w-2)), [2 1]);
+wj_2 = repmat(wj_2(:)', [1 h]);
+wj_3 = repmat(0:(h - 1), [(2*(w-1)) 1]);
+wj_3 = wj_3(:)';
+wj = wj_1 + wj_2 + wj_3; 
+
+wv = repmat(x, [1 x_w]);
+
+ci = [hi wi];
+cj = [hj wj];
+cv = [hv wv];
 
 % collect triplets here
-
-G = sparse(i,j,v);
+G = sparse(wi,wj,wv);
+%G = sparse(ci,cj,cv);
 
 
--- myTask.m
+++ myTask.m
@@ -1,12 +1,24 @@
-image=imread('lena_gray.png');
-[h w d]=size(image);
+clear all
+close all
+
+image=imread('test.png');
+image=imread('blurryImage.png');
+figure, imshow(image)
+[h, w, d]=size(image);
 U = double(reshape(image,w*h,d))/255;
 
 %% Write your method here
+c_s = 3.0;
+c_U = 0.5;
+
+G = gradient(h, w);
+
+g = (0.5*G)*U;
+
+U_new = ((G'*G + c_U* speye(h*w)))\(c_s*G'*g + c_U*U);
 
 
-
-image =uint8(reshape(U,h,w,d)*255);
+image =uint8(reshape(U_new,h,w,d)*255);
 
 figure, imshow(image)
 imwrite(image,'out.png')
